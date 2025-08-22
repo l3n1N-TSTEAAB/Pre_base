84559a7268c141cff2e7121d57316c6e7831eb74
l3n1N-TSTEAAB <lenin.gdeleon@gmail.com>
2025-08-22 17:35:31 -0600

    inicio

--- Pre.py
+++ Pre.py
@@ -1,1 +1,12 @@
 ##inicio
+
+
+class Productos():
+    def __init__(self,Nombre,Precio,IDCategoria,TotalCompras,TotalVentas,Stock):
+        self.Nombre = Nombre
+        self.Precio = Precio
+        self.IDCategoria = IDCategoria
+        self.TotalCompras = TotalCompras
+        self.TotalVentas = TotalVentas
+        self.Stock = Stock
+
